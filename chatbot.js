39dbdee90b67355084e87cd153fe2f0a79f1efd6
Ahmoray Arana <bebegirl217@gmail.com>
2016-09-28 14:38:01 -0700

    Adding the time

--- chatbot.js
+++ chatbot.js
@@ -1,14 +1,21 @@
 var question;
 var answer;
 
+  var now = new Date(Date.now());
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+
 var questionConversion = {
   "how old are you?": "Younger than you senior citizen",
+  "are you left or right handed?": "I'm neither I'm a coder",
+  "what is your favorite food?": "I don't have a favorite food",
+  "what would you consider yourself?": "I'm messy and live for drama",
+  "are you a people person?": "Nope, I don't like people",
   "what is your name?": "Diamond like Rihanna's song",
   "what is your favorite color?": "I love blue like the sky",
   "who is your favorite artist?": "I love Beyonce, Rihanna, and YG",
-  "have you ever seen twilight?":"Yaasss that is my cup of tea",
-
-  //"what time is it?"://
+  "have you ever seen twilight?": "Yaasss that is my cup of tea",
+  "what time is it?": hours + ":" + minutes
 }
 
 
@@ -31,6 +38,9 @@ function speak() {
 
     $("#chat-area").prepend(answer + "<br>");
   }
+
+  var now = new Date(Date.now());
+
 
 
 }
