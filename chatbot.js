b717e5b0cf66e017afda49d8091c43f4b9a9afbc
Ahmoray Arana <bebegirl217@gmail.com>
2016-09-30 14:26:17 -0700

    Adding Extra Credit/CSS

--- chatbot.js
+++ chatbot.js
@@ -6,16 +6,16 @@ var answer;
   var minutes = now.getMinutes();
 
 var questionConversion = {
-  "how old are you?": "Younger than you senior citizen",
-  "are you left or right handed?": "I'm neither I'm a coder",
-  "what is your favorite food?": "I don't have a favorite food",
-  "what would you consider yourself?": "I'm messy and live for drama",
-  "are you a people person?": "Nope, I don't like people",
-  "what is your name?": "Diamond like Rihanna's song",
-  "what is your favorite color?": "I love blue like the sky",
-  "who is your favorite artist?": "I love Beyonce, Rihanna, and YG",
-  "have you ever seen twilight?": "Yaasss that is my cup of tea",
-  "what time is it?": hours + ":" + minutes
+  "how old are you?": ["Younger than you senior citizen", "A women never reveals her age"],
+  "are you left or right handed?": ["I'm neither I'm a coder", "Right handed"],
+  "what is your favorite food?": ["I don't have a favorite food", "I don't have a favorite food but I hate certain foods"],
+  "what would you consider yourself?": ["I'm messy and live for drama ~ Joanne the Scammer"],
+  "are you a people person?": ["Nope, I don't like people", "I don't like people who are annoying"],
+  "what is your name?": ["Diamond like Rihanna's song"],
+  "what is your favorite color?": ["I love blue like the sky", ""],
+  "who is your favorite artist?": ["I love Beyonce, Rihanna, and YG", "Me, Myself, and I"],
+  "have you ever seen twilight?": ["Yaasss that is my cup of tea"],
+  "what time is it?": [hours + ":" + minutes]
 }
 
 
@@ -27,7 +27,10 @@ function speak() {
 
   $("#chat-area").prepend(question + "<br>");
 
-  answer = questionConversion[question];
+  var answerArray = questionConversion[question];
+  var answer =  Math.floor((Math.random() * answerArray.length));
+
+
 
   if(answer == undefined) {
 
@@ -41,7 +44,10 @@ function speak() {
     $("#chat-area").prepend(answer + "<br>");
   }
 
-
-
-
 }
+
+$(document).keydown(function(e) {
+ if (e.keyCode == 13) {
+   speak();
+ }
+});
